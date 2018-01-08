8dcb983f73d63e875619f2273ac38149d8d93c3f
Bhavya Bharti <bhavyab@outlook.com>
2018-01-08 15:15:24 -0800

    reload working

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -1,13 +1,16 @@
 'use strict'
 import React from 'react'
 import {Provider} from 'react-redux'
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Link} from 'react-router-dom'
 
 import createAppStore from '../../lib/store.js'
 import Landing from '../landing'
 
 const store = createAppStore()
-
+import One from '../one'
+import Two from '../two'
+import Three from '../three'
+ 
 class App extends React.Component {
   constructor(props){
     super(props)
@@ -18,9 +21,24 @@ class App extends React.Component {
       <section>
         <Provider store={store}>
           <BrowserRouter>
-            <section>
-              <Route exact path='/' component={Landing} />
-            </section>
+          <div>
+            <header>
+              <h1> cool logo </h1>
+              <nav>
+                <ul>
+                  <li><Link to='/'> Landing </Link> </li>
+                  <li><Link to='/page1'> Page-1 </Link> </li>
+                  <li><Link to='/page2'>  Page-2 </Link> </li>
+                  <li><Link to='/page3'>  Page-3 </Link> </li>
+                </ul>
+              </nav>
+            </header>
+
+            <Route exact path='/page1' component={One} />
+            <Route exact path='/page2' component={Two} />
+            <Route exact path='/page3' component={Three} />
+            <Route exact path='/' component={Landing} />
+          </div>
           </BrowserRouter>
         </Provider>
       </section>
--- src/component/landing/index.js
+++ src/component/landing/index.js
@@ -2,11 +2,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {renderIf} from '../../lib/util'
-import {BrowserRouter, Route, Link} from 'react-router-dom'
 
-import One from '../one'
-import Two from '../two'
-import Three from '../three'
 
 class Landing extends React.Component {
   constructor(props){
@@ -18,24 +14,6 @@ class Landing extends React.Component {
     return (
       <section className='landing'>
           <div>Landing Page</div>
-          <BrowserRouter>
-          <div>
-            <header>
-              <h1> cool swee awesome yeeee </h1>
-              <nav>
-                <ul>
-                  <li><Link to='/page1'> Page-1 </Link> </li>
-                  <li><Link to='/page2'>  Page-2 </Link> </li>
-                  <li><Link to='/page3'>  Page-3 </Link> </li>
-                </ul>
-              </nav>
-            </header>
-
-            <Route exact path='/page1' component={One} />
-            <Route exact path='/page2' component={Two} />
-            <Route exact path='/page3' component={Three} />
-          </div>
-          </BrowserRouter>
       </section>
     )
   }
