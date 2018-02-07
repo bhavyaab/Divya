2e7b9191c1120d5f1695aeba90dc5ced5fa236c0
Bhavya Bharti <bhavyab@outlook.com>
2018-02-07 12:27:46 -0800

    book appointement

--- src/component/about/index.js
+++ src/component/about/index.js
@@ -9,7 +9,19 @@ class About extends React.Component {
   constructor(props){
     super(props)
     this.state = {}
+    this.scrollFunction = this.scrollFunction.bind(this)
   }
+  
+  scrollFunction(x, e) {
+    e.preventDefault();
+    document.getElementById(x).scrollIntoView({ 
+    behavior: 'smooth',
+    block: "end",
+    inline: "nearest"
+  });  
+  }
+  
+  // <button className="inAbout" onClick={this.scrollFunction.bind(this, 'contact')}>BOOK NOW</button>
   render(){
       return (
       <section id="about" className='about'>
@@ -17,7 +29,6 @@ class About extends React.Component {
           <h2>About Us</h2>
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into 
           electronic typesetting, remaining essentially unchanged.</p>
-          <button className="inAbout">BOOK NOW</button>
         </div>
       </section>
     )
