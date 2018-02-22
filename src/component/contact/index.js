5cbdeaec30ba712800dc4985ea92402e40102c38
Bhavya <bhavyab@outlook.com>
2018-02-22 13:51:17 -0800

    sdfsdf

--- src/component/contact/index.js
+++ src/component/contact/index.js
@@ -27,7 +27,7 @@ class Contact extends React.Component {
               <ul>
                 <li><span>Address:</span><div>{this.props.contact.address}   <br /><a href={this.props.contact.googleMap} target="blank"> view map  <i className="fas fa-share"></i></a></div></li>
                 <li><span>Call:</span><div>{this.props.contact.mobile}</div></li>
-                <li><span>Email:</span> <div>{this.props.contact.email}</div></li>
+                <li><span>Email:</span> <div><a href="mailto:divyavikrant@gmail.com?subject=Request%20a%20Service">{this.props.contact.email}</a></div></li>
                </ul>
             </div>
           </div>
